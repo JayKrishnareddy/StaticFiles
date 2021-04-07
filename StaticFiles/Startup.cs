09518cacf46da65e33355c8c7ab3cb0196391e5a
Jay Krishna Reddy <majji.jaya_krishna_reddy@nokia.com>
2021-04-07 12:28:49 +0530

    Final

--- StaticFiles/Startup.cs
+++ StaticFiles/Startup.cs
@@ -4,11 +4,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -43,7 +45,13 @@ namespace StaticFiles
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StaticFiles v1"));
             }
-
+            app.UseFileServer(new FileServerOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "StaticFiles")),
+                RequestPath = "/StaticFiles",
+                EnableDefaultFiles = true
+            }) ;
             app.UseHttpsRedirection();
 
             app.UseRouting();
---
+++ StaticFiles/StaticFiles/main.js
@@ -0,0 +1,1 @@
+﻿alert("Triggered URL");
